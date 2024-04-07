48d0a02cda3f43d6401f65f1ba91633a2212a030
woong8249 <woong8249@gmail.com>
2024-04-07 20:54:15 +0900

    Add bugs test code

---
+++ back/test/bugs.test.js
@@ -0,0 +1,74 @@
+import {
+  describe,
+  expect, it,
+} from 'vitest';
+
+import { fetchChart, fetchChartsForDateRangeInParallel } from '../dataCollecting/bugs.js';
+import { extractYearMonthDay } from '../dataCollecting/util.js';
+
+import { getRandomDateRange, moveToNearestFutureDay } from './util.js';
+
+describe('The fetchChart func Test', () => {
+  it('The Bugs daily chart has been available since September 22, 2006. So fetchChart(\'2006\', \'03\', \'21\', \'day\') is going to throw Error.', () => {
+    expect(() => fetchChart('2006', '03', '21', 'day')).rejects.toThrowError('The Bugs daily chart has been available since September 22, 2006.');
+    expect.assertions(1);
+  });
+
+  it('The Bugs daily chart has been available since September 22, 2006. It will test using a randomly selected Monday after this date.', async () => {
+    const minDate = new Date('2006-03-22');
+    const today = new Date();
+    const { startDate: _startDate } = getRandomDateRange(minDate, today, 1);
+    const testTarget = moveToNearestFutureDay(_startDate, 1);
+    const { year, month, day } = extractYearMonthDay(testTarget);
+    const { chartDetails, chartScope } = await fetchChart(year, month, day, 'day');
+    const { chartType } = chartScope;
+
+    expect(chartType).toBe('day');
+    expect(chartDetails[0]).toHaveProperty('rank');
+    expect(chartDetails[0]).toHaveProperty('artist');
+    expect(chartDetails[0]).toHaveProperty('title');
+    expect.assertions(4);
+  });
+
+  it('The Bugs weekly chart has been available since August 29, 2003. So fetchChart(\'2003\', \'08\', \'28\', \'week\') is going to throw Error.', () => {
+    expect(() => fetchChart('2003', '08', '28', 'week')).rejects.toThrowError('The Bugs weekly chart has been available since August 29, 2003.');
+    expect.assertions(1);
+  });
+
+  it('The Bugs monthly chart has been available since January 1, 2010. We will test using a randomly selected first day of month after this date.', async () => {
+    const minDate = new Date('2003-08-29');
+    const today = new Date();
+    const { startDate: _startDate } = getRandomDateRange(minDate, today, 1);
+    const testTarget = new Date(_startDate.setDate(1));
+    const { year, month, day } = extractYearMonthDay(testTarget);
+    const { chartDetails, chartScope } = await fetchChart(year, month, day, 'week');
+    const { chartType } = chartScope;
+
+    expect(chartType).toBe('week');
+    expect(chartDetails[0]).toHaveProperty('rank');
+    expect(chartDetails[0]).toHaveProperty('artist');
+    expect(chartDetails[0]).toHaveProperty('title');
+    expect.assertions(4);
+  });
+});
+
+describe('fetchChartsForDateRangeInParallel', () => {
+  it('It can obtain daily charts for a specific period.', async () => {
+    const result = await fetchChartsForDateRangeInParallel(new Date('2024-02-21'), new Date('2024-03-01'), 'day');
+    const { chartScope: chartScope2M } = result.find(item => item.chartScope.date.getDate() === new Date('2024-02-01').getDate());
+    const { chartScope: chartScope3M } = result.find(item => item.chartScope.date.getDate() === new Date('2024-03-01').getDate());
+    expect(result.length).toBe(10);
+    expect(chartScope2M.date.getTime() === new Date('2024-02-21').getDate());
+    expect(chartScope3M.date.getTime() === new Date('2024-03-01').getDate());
+    expect.assertions(3);
+  });
+  it('It can obtain weekly charts for a specific period.', async () => {
+    const result = await fetchChartsForDateRangeInParallel(new Date('2024-02-05'), new Date('2024-02-16'), 'week');
+    const { chartScope: chartScope2M2W } = result.find(item => item.chartScope.startDate.getDate() === new Date('2024-02-05').getDate());
+    const { chartScope: chartScope2M3W } = result.find(item => item.chartScope.startDate.getDate() === new Date('2024-02-12').getDate());
+    expect(result.length).toBe(2);
+    expect(chartScope2M2W.startDate.getTime() === new Date('2024-02-05').getDate());
+    expect(chartScope2M3W.startDate.getTime() === new Date('2024-02-12').getDate());
+    expect.assertions(3);
+  });
+});
