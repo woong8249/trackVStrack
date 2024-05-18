ef3ffbd70a07181c480dd4d5d8ec82ce90f737be
woong8249 <woong8249@gmail.com>
2024-05-18 13:11:38 +0900

    add rateLimit

--- apiServer/src/express/routes/artist.js
+++ apiServer/src/express/routes/artist.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 
 import * as artist from '../controller/artist.js';
 import * as validator from '../validator/index.js';
 
 const router = express.Router();
 
-router.get('/:id', validator.validateParmId, artist.getArtistWithTrack);
-router.get('/', validator.validateQueryQ, artist.getRelatedArtists);
+// 테스트코드 필요
+const getArtistLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 50,
+  message: 'Too many requests from this IP, please try again after 10 minutes',
+});
+
+const getRelatedArtistsLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 200, //
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+router.get('/:id', validator.validateParmId, getArtistLimiter, artist.getArtistWithTrack);
+router.get('/', validator.validateQueryQ, getRelatedArtistsLimiter, artist.getRelatedArtists);
 
 export default router;
--- apiServer/src/express/routes/track.js
+++ apiServer/src/express/routes/track.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 
 import * as trackController from '../controller/track.js';
 import * as validator from '../validator/index.js';
 
 const router = express.Router();
 
-router.get('/:id', validator.validateParmId, trackController.getTrackWithArtist);
-router.get('/', validator.validateQueryQ, trackController.getRelatedTracks);
+// 테스트코드 필요
+const getTrackLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 50,
+  message: 'Too many requests from this IP, please try again after 10 minutes',
+});
+
+const getRelatedTracksLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 200, //
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+router.get('/:id', validator.validateParmId, getTrackLimiter, trackController.getTrackWithArtist);
+router.get('/', validator.validateQueryQ, getRelatedTracksLimiter, trackController.getRelatedTracks);
 
 export default router;
