9583b5883631df733b19f6045fb85320450562a6
woong8249 <woong8249@gmail.com>
2024-09-21 23:58:02 +0900

    Fix to Melon class

--- ts-crawling/src/types/index.ts
+++ ts-crawling/src/types/index.ts
@@ -1,4 +1,5 @@
 export type ChartType ='m' |'w'|'n'
+export type PlatformName ='melon' | 'genie' | 'bugs'
 
 export type WeekOfMonth ={
     year: string;
@@ -12,10 +13,20 @@ export type Track ={
     titleKeyword:string
     trackID:string
 }
+export type TrackAddInfo ={
+    releaseDate:Date
+    trackImage:string
+    lyrics: string
+}
+
 export type Artist ={
     artistName:string
     artistKeyword:string
     artistID: string
+}
+export type ArtistAddInfo ={
+    artistImage:string
+    debut: string
 }
 
 export type ChartDetail = Track &{artists:Artist[]}
@@ -30,4 +41,24 @@ export type WeeklyChartScope ={
 export type MonthlyChartScope ={
     date :Date
     chartType:ChartType
+}
+
+export type FetchWeeklyChartResult ={
+    platform: PlatformName
+    chartScope:WeeklyChartScope
+    chartDetails:ChartDetail[]
+}
+
+export type FetchMonthlyChartResult ={
+  platform: PlatformName
+  chartScope:MonthlyChartScope
+  chartDetails:ChartDetail[]
+}
+
+export interface PlatformModule {
+    readonly platformName:PlatformName
+    fetchChart(year:string, month:string, day:string, chartType:ChartType):Promise<FetchMonthlyChartResult|FetchWeeklyChartResult>
+    fetchChartsInParallel(startDate:Date, endDate:Date, chartType:ChartType, chunkSize :number):Promise<(FetchMonthlyChartResult|FetchWeeklyChartResult)[]>
+    fetchAddInfoOfTrack(trackID:string):Promise<TrackAddInfo>
+    fetchAddInfoOArtist(artistID:string): Promise<ArtistAddInfo>
 }
