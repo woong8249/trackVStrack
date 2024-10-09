07472a3ddc5a512848ca801186af4e98a1f40ec7
woong8249 <woong8249@gmail.com>
2024-10-09 09:15:38 +0900

    Add performance improvement using position

--- react-client/src/components/TrackOverview.tsx
+++ react-client/src/components/TrackOverview.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useRef, useState } from 'react';
 import { Resizable, ResizeCallbackData } from 'react-resizable';
 import { useModal } from '@hooks/useModal';
 import ChartGraph from './ChartGraph';
@@ -16,14 +16,27 @@ interface Props {
 export default function TrackOverview({ track, viewportType }: Props) {
   const { isModalOpen, setIsModalOpen, modalRef } = useModal();
   const [width, setWidth] = useState(viewportType !== 'small' ? 600 : 400); // width를 상태로 관리
-
-  // width를 기준으로 height와 isLargeContainer를 계산
   const height = (width >= 600) && viewportType !== 'small' ? 130 + (width / 1.6) : 100;
   const isLargeContainer = width >= 600;
+  const [isResizing, setIsResizing] = useState(false);
+  const initialWidth = useRef(width);
+  const initialHeight = useRef(height);
+  const zIndex = isResizing ? 1 : 1;
+  const position: 'relative' | 'absolute' = isResizing ? 'absolute' : 'relative';
+
+  const handleResizeStart = () => {
+    initialWidth.current = width; // 리사이즈 시작 시 width 고정
+    initialHeight.current = height; // 리사이즈 시작 시 height 고정
+    setIsResizing(true); // 리사이즈 중임을 상태로 관리
+  };
 
   const onResize = (_e: SyntheticEvent, { size }: ResizeCallbackData) => {
     const newWidth = size.width;
     setWidth(newWidth); // width만 상태로 관리
+  };
+
+  const handleResizeStop = () => {
+    setIsResizing(false);
   };
 
   const handleModalOpen = () => {
@@ -52,7 +65,7 @@ export default function TrackOverview({ track, viewportType }: Props) {
 
         {/* Modal */}
         {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-15 backdrop-blur-sm">
+        <div className="fixed inset-0 z-[1000] flex items-center justify-center bg-black bg-opacity-15 backdrop-blur-sm">
           <div
       ref={modalRef}
       className="bg-white rounded-lg p-4 relative max-h-[auto] w-[auto] overflow-auto"
@@ -69,53 +82,83 @@ export default function TrackOverview({ track, viewportType }: Props) {
         </div>
         )}
       </div>
-    ) : (
-      <div className="border-[1px] border-[#9A9A9A] rounded-m transition shadow-2xl relative">
-        <Resizable
-        width={width}
-        height={height}
-        onResize={onResize}
-        minConstraints={[400, 100]}
-        maxConstraints={[800, 500]}
-        lockAspectRatio={false}
-      >
-          <div style={{ width, height }}>
-            <TrackInfoCard track={track} />
+    )
+      : (
+        <div style={{ position: 'relative' }}>
+          {isResizing && (
+          <div
+            style={{
+              width: `${initialWidth.current}px`, // 리사이즈 시작 시의 고정된 width
+              height: `${initialHeight.current}px`, // 리사이즈 시작 시의 고정된 height
+              display: 'inline-block', // 원래 자리 차지
+              visibility: 'hidden', // 공간을 차지하되 보이지 않도록 설정
+            }}
+          />
+          )}
 
-            { isLargeContainer && (
-            <>
-              <div className="border-b-[1px] border-[#9A9A9A] mb-[1rem]"></div>
-              <ChartGraph track={track} />
-            </>
+          <div
+            className="border-[1px] border-[#9A9A9A] rounded-m  shadow-2xl"
+            style={{
+              position, // 리사이징 중일 때는 absolute로 설정
+              zIndex, // 리사이징 중일 때 더 높은 레이어에 위치
+              top: position === 'absolute' ? '0' : undefined,
+              left: position === 'absolute' ? '0' : undefined,
+              width: `${width}px`,
+              height: `${height}px`,
+              // transition: 'all 0.1s ease', // 애니메이션 속도 설정
+            }}
+          >
+            <Resizable
+              width={width}
+              height={height}
+              onResize={onResize}
+              onResizeStart={handleResizeStart} // 리사이즈 시작
+              onResizeStop={handleResizeStop} // 리사이즈 끝
+              minConstraints={[400, 100]}
+              maxConstraints={[800, 500]}
+              lockAspectRatio={false}
+            >
+              <div
+                style={{
+                  width: '100%', height: '100%',
+                }}>
+                <TrackInfoCard track={track} />
+
+                { isLargeContainer && (
+                <>
+                  <div className="border-b-[1px] border-[#9A9A9A] mb-[1rem]"></div>
+                  <ChartGraph track={track} />
+                </>
+                )}
+              </div>
+            </Resizable>
+
+            <div className="absolute top-2 right-2">
+              <button onClick={handleModalOpen} className="bg-transparent text-green-600 p-2 rounded-full hover:text-green-800">
+                <FaExpandAlt size={20} />
+              </button>
+            </div>
+
+            {/* Modal */}
+            {isModalOpen && (
+            <div className="fixed inset-0 z-999 flex items-center justify-center bg-black bg-opacity-15 backdrop-blur-sm">
+              <div
+                ref={modalRef}
+                className="bg-white z-999 rounded-lg p-4 relative max-h-[auto] w-[auto] overflow-auto"
+                onClick={(e) => e.stopPropagation()}
+                role="button"
+                tabIndex={0}
+              >
+                <div className="w-[50rem] sm:w-[70rem] overflow-auto">
+                  <TrackInfoCard track={track} />
+                  <div className="border-b-[1px] border-[#9A9A9A] mb-[1rem]"></div>
+                  <ChartGraph track={track} />
+                </div>
+              </div>
+            </div>
             )}
           </div>
-        </Resizable>
-
-        <div className="absolute top-2 right-2">
-          <button onClick={handleModalOpen} className="bg-transparent text-green-600 p-2 rounded-full hover:text-green-800">
-            <FaExpandAlt size={20} />
-          </button>
         </div>
-
-        {/* Modal */}
-        {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-15 backdrop-blur-sm">
-          <div
-            ref={modalRef}
-            className="bg-white rounded-lg p-4 relative max-h-[auto] w-[auto] overflow-auto"
-            onClick={(e) => e.stopPropagation()}
-            role="button"
-            tabIndex={0}
-          >
-            <div className="w-[50rem] sm:w-[70rem] overflow-auto">
-              <TrackInfoCard track={track} />
-              <div className="border-b-[1px] border-[#9A9A9A] mb-[1rem]"></div>
-              <ChartGraph track={track} />
-            </div>
-          </div>
-        </div>
-        )}
-      </div>
-    )
+      )
   );
 }
