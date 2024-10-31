5459f9b4336e46886641221dd114fdfd636e1600
woong8249 <woong8249@gmail.com>
2024-11-01 04:19:41 +0900

    Add ErrorPage

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -11,21 +11,23 @@ import HomePage from '@pages/HomePage';
 import ExplorePage from '@pages/ExplorePage';
 import { State, SWRConfig } from 'swr';
 import { fetcher } from '@utils/axios';
+import ErrorPage from '@pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/explore',
     element: <ExplorePage />,
+    errorElement: <ErrorPage />,
   },
 ]);
 const persistentProvider = () => {
   const cachedData = JSON.parse(sessionStorage.getItem('trackVStrack-swr-cache') || '[]');
   const map = new Map<string, State>(cachedData);
-  console.log(map);
   // 페이지가 닫히거나 새로고침될 때, sessionStorage에 현재 캐시 저장
   window.addEventListener('beforeunload', () => {
     sessionStorage.setItem('trackVStrack-swr-cache', JSON.stringify(Array.from(map.entries())));
@@ -35,14 +37,13 @@ const persistentProvider = () => {
 };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-
   <SWRConfig
-  value={{
-    fetcher: ([url, parm]) => fetcher(url, parm),
-    provider: persistentProvider,
-    revalidateOnFocus: false, //  창이 포커싱되었을 때 자동 갱신 x
-    revalidateIfStale: false, // 오래된 데이터가 있더라도 자동으로 다시 확인 x
-  }}
+    value={{
+      fetcher: ([url, parm]) => fetcher(url, parm),
+      provider: persistentProvider,
+      revalidateOnFocus: false, //  창이 포커싱되었을 때 자동 갱신 x
+      revalidateIfStale: false, // 오래된 데이터가 있더라도 자동으로 다시 확인 x
+    }}
   >
     <RouterProvider router={router} />
   </SWRConfig>,
