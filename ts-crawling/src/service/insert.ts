f389a440a9eb956fe37f46cf5e6bb1d2eb0e7549
woong8249 <woong8249@gmail.com>
2024-12-09 18:51:40 +0900

    add eslint-disable

--- ts-crawling/src/service/insert.ts
+++ ts-crawling/src/service/insert.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable no-await-in-loop */
