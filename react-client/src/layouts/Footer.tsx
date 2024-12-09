760856cfa38efa725cdb43f9dcbfed1bbaac30b3
woong8249 <woong8249@gmail.com>
2024-12-09 14:51:44 +0900

    Fix footer tag

--- react-client/src/layouts/Footer.tsx
+++ react-client/src/layouts/Footer.tsx
@@ -1,35 +1,31 @@
 export function Footer() {
   return (
-    <>
-      <footer className=" text-gray-400 py-6">
-        <div className="max-w-5xl mx-auto px-4 text-center space-y-4">
-          {/* 서비스 소개 */}
-          <p className="text-sm">
-            This service helps you easily compare and analyze platform chart performance.
-          </p>
+    <footer className="text-gray-400 py-6">
+      <div className="max-w-5xl mx-auto px-4 text-center space-y-4">
+        {/* 서비스 소개 */}
+        <p className="text-sm">
+          This service helps you easily compare and analyze platform chart performance.
+        </p>
 
-          {/* 연락처 정보 */}
-          <p className="text-sm">
-            📧 Contact:
-            {' '}
+        {/* 연락처 정보 */}
+        <div className="text-sm">
+          📧 Contact:
+          {' '}
 
-            <p
-              className=" text-gray-600 underline"
-            >
-              woong8249@gmail.com
-            </p>
-          </p>
-
-          {/* 저작권 정보 */}
-          <p className="text-sm">
-            ©
-            {' '}
-            {new Date().getFullYear()}
-            {' '}
-            Jiwoong Hwang. All rights reserved.
-          </p>
+          <span className="text-gray-600 underline">
+            woong8249@gmail.com
+          </span>
         </div>
-      </footer>
-    </>
+
+        {/* 저작권 정보 */}
+        <p className="text-sm">
+          ©
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          Jiwoong Hwang. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
 }
